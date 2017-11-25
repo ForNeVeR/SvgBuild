ccca92c72584d8420fe006cceda7292daa2da815
Friedrich von Never <friedrich@fornever.me>
2017-11-25 16:12:52 +0700

    SvgBuild.Console: initial proof-of-concept

---
+++ Readme.md
@@ -0,0 +1,23 @@
+SvgBuild
+========
+
+This is a .NET based tool set to save SVG to raster images. Includes a
+terminal-based program.
+
+Build
+-----
+
+This program requires [.NET Core SDK 2.0][net-sdk] to build it.
+
+```console
+$ dotnet build
+```
+
+Run
+---
+
+```console
+$ dotnet run --project SvgBuild.Console [path to input file] [path to output file]
+```
+
+[net-sdk]: https://www.microsoft.com/net/download
---
+++ SvgBuild.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using Svg;
+
+namespace SvgBuild.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = args[0];
+            var output = args[1];
+            var d = SvgDocument.Open(input);
+            var i = new Bitmap((int)d.Width.Value, (int)d.Height.Value);
+            d.Draw(i);
+            i.Save(output);
+        }
+    }
+}
