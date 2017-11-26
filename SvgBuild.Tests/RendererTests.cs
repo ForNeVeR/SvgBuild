8d8e887c098308e2cf231ffd0d5358e853684feb
Friedrich von Never <friedrich@fornever.me>
2017-11-26 16:10:23 +0700

    #7: add file format support (no .ico still)

--- SvgBuild.Tests/RendererTests.cs
+++ SvgBuild.Tests/RendererTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -10,21 +11,48 @@ namespace SvgBuild.Tests
     public class RendererTests
     {
         [Fact]
-        public void RendererThorwsArgumentExceptionIfInputOrOutputIsNull()
+        public void RendererThorwsArgumentNullExceptionIfInputOrOutputIsNull()
         {
             Assert.Throws<ArgumentNullException>(() => Renderer.Render(null, "xxx"));
             Assert.Throws<ArgumentNullException>(() => Renderer.Render("xxx", null));
         }
 
         [Fact]
-        public async Task RendererRendersSimpleImage()
+        public void RendererThorwsArgumentExceptionIfOutputHasInvalidFormat()
         {
-            var path = await SvgUtilities.CreateTempImage();
-            var output = Path.GetTempFileName();
-            Renderer.Render(path, output);
+            Assert.Throws<ArgumentException>(() => Renderer.Render("file.svg", "file.txt"));
+        }
+
+        [Fact]
+        public Task RendererCreatesBmpImage() => AssertRenderedImageFormat("bmp", ImageFormat.Bmp);
+
+        [Fact]
+        public Task RendererCreatesGifImage() => AssertRenderedImageFormat("gif", ImageFormat.Gif);
+
+        [Fact]
+        public Task RendererCreatesIcoImage() => AssertRenderedImageFormat("ico", ImageFormat.Icon);
+
+        [Fact]
+        public async Task RendererCreatesJpegImages()
+        {
+            await AssertRenderedImageFormat("jpg", ImageFormat.Jpeg);
+            await AssertRenderedImageFormat("jpeg", ImageFormat.Jpeg);
+        }
+
+        [Fact]
+        public Task RendererCreatesPngImage() => AssertRenderedImageFormat("png", ImageFormat.Png);
+
+        [Fact]
+        public Task RendererCreatesTiffImage() => AssertRenderedImageFormat("tiff", ImageFormat.Tiff);
+
+        private async Task AssertRenderedImageFormat(string outputFileExtension, ImageFormat format)
+        {
+            var input = await SvgUtilities.CreateTempImage();
+            var output = Path.ChangeExtension(Path.GetTempFileName(), outputFileExtension);
+            Renderer.Render(input, output);
 
             var image = Image.FromFile(output);
-            Assert.IsType<Bitmap>(image);
+            Assert.Equal(format, image.RawFormat);
         }
     }
 }
--- SvgBuild/Renderer.cs
+++ SvgBuild/Renderer.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using Svg;
 
 namespace SvgBuild
 {
     public static class Renderer
     {
+        private static readonly Dictionary<string, ImageFormat> _extensionFormats = new Dictionary<string, ImageFormat>
+        {
+            [".bmp"] = ImageFormat.Bmp,
+            [".gif"] = ImageFormat.Gif,
+            [".ico"] = ImageFormat.Icon,
+            [".jpeg"] = ImageFormat.Jpeg,
+            [".jpg"] = ImageFormat.Jpeg,
+            [".png"] = ImageFormat.Png,
+            [".tiff"] = ImageFormat.Tiff,
+        };
+
         public static void Render(string inputPath, string outputPath)
         {
             if (inputPath == null)
@@ -18,13 +33,26 @@ namespace SvgBuild
                 throw new ArgumentNullException(nameof(outputPath));
             }
 
+            var format = DetermineOutputFormat(Path.GetExtension(outputPath));
+            if (format == null)
+            {
+                throw new ArgumentException(
+                    $"Cannot determine output file format based on the file extension {Path.GetExtension(outputPath)}",
+                    nameof(outputPath));
+            }
+
             var document = SvgDocument.Open(inputPath);
             var size = document.GetDimensions().ToSize();
             using (var image = new Bitmap(size.Width, size.Height))
             {
                 document.Draw(image);
-                image.Save(outputPath);
+                image.Save(outputPath, format);
             }
+        }
+
+        private static ImageFormat DetermineOutputFormat(string extension)
+        {
+            return _extensionFormats.TryGetValue(extension, out var format) ? format : null;
         }
     }
 }
