3970e554cba74130b1338d1846bdcbcd3f3880b4
Friedrich von Never <friedrich@fornever.me>
2017-12-31 21:29:43 +0700

    Release 1.0.0

--- SvgBuild.Console/Properties/AssemblyInfo.cs
+++ SvgBuild.Console/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C) 2017 F. von Never")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6e3e75ab-3bd0-452c-8658-9ffca8c6b882")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- SvgBuild.MsBuild/Properties/AssemblyInfo.cs
+++ SvgBuild.MsBuild/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C) 2017 F. von Never")]
 [assembly: ComVisible(false)]
 [assembly: Guid("108845ee-a720-411f-9317-8e1381736337")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
